37891d111fcf64a1522a193fe3063d9e230f3be3
DavidCagua <felipe0160@gmail.com>
2022-05-17 11:29:31 -0500

    react webapp added

---
+++ web-app/src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+import { Button } from "@amobladospastods/components";
+function App() {
+  return (
+    <div className="App">
+      <Button label="hola" />
+    </div>
+  );
+}
+
+export default App;
